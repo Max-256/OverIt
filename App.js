45c4ec64a523a4d35c9da0d90b8cf9ce883246b1
Opio Maxwell <opiomaxwell11122@gmail.com>
2023-04-18 00:40:45 +0300

    build the button component

--- App.js
+++ App.js
@@ -2,9 +2,19 @@ import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
 import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
+import AppButton from "./app/components/AppButton";
+import colors from "./app/config/colors";
 
 export default function App() {
-  return <ViewImageScreen />;
+  const handlePress = () => {
+    console.log("button Pressed");
+  };
+
+  return (
+    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+      <AppButton title="login" onPress={handlePress} color={colors.primary} />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({});
---
+++ app/components/AppButton.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { View, Text, StyleSheet, TouchableOpacity } from "react-native";
+import colors from "../config/colors";
+
+function AppButton({ title, onPress, color }) {
+  return (
+    <TouchableOpacity
+      onPress={onPress}
+      style={[styles.button, { backgroundColor: color }]}
+    >
+      <Text style={styles.text}>{title}</Text>
+    </TouchableOpacity>
+  );
+}
+
+const styles = StyleSheet.create({
+  button: {
+    width: "100%",
+    padding: 8,
+    borderRadius: 25,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    textTransform: "uppercase",
+    color: colors.white,
+    fontSize: 15,
+    fontWeight: "bold",
+  },
+});
+
+export default AppButton;
