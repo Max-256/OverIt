9a1850237edc9909fea9fcdd55aadc95e858994d
Opio Maxwell <opiomaxwell11122@gmail.com>
2023-04-17 20:44:34 +0300

    add view Image Screen

---
+++ app/screens/ViewImageScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+function ViewImageScreen(props) {
+  return (
+    <View>
+      <Text>View image screen</Text>
+    </View>
+  );
+}
+
+export default ViewImageScreen;
