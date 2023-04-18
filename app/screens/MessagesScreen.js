8d3d23d28c61091d7cbbd9d10ba28f8399febfa8
Opio Maxwell <opiomaxwell11122@gmail.com>
2023-04-18 16:45:11 +0300

    extract the screen component

--- app/screens/MessagesScreen.js
+++ app/screens/MessagesScreen.js
@@ -1,12 +1,65 @@
 import React from "react";
-import { View, Text } from "react-native";
+import { FlatList } from "react-native";
+import { View, Text, StyleSheet } from "react-native";
+import ListItem from "../components/ListItem";
+import Constants from "expo-constants";
+import Screen from "../components/Screen";
+
+const messages = [
+  {
+    id: 1,
+    title: "T1",
+    description: "d1",
+    image: require("../assets/mosh.jpg"),
+  },
+  {
+    id: 2,
+    title: "T2",
+    description: "d2",
+    image: require("../assets/mosh.jpg"),
+  },
+  {
+    id: 3,
+    title: "T3",
+    description: "d3",
+    image: require("../assets/mosh.jpg"),
+  },
+  {
+    id: 4,
+    title: "T4",
+    description: "d4",
+    image: require("../assets/mosh.jpg"),
+  },
+  {
+    id: 5,
+    title: "T5",
+    description: "d5",
+    image: require("../assets/mosh.jpg"),
+  },
+];
 
 function MessagesScreen(props) {
   return (
-    <View>
-      <Text>Messages Screen</Text>
-    </View>
+    <Screen>
+      <FlatList
+        data={messages}
+        keyExtractor={(message) => message.id.toString()}
+        renderItem={({ item }) => (
+          <ListItem
+            title={item.title}
+            subtitle={item.description}
+            image={item.image}
+          />
+        )}
+      />
+    </Screen>
   );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    paddingTop: Constants.statusBarHeight,
+  },
+});
 
 export default MessagesScreen;
