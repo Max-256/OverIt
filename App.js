d6786a2d63b835dda9f0db83a9578e5b3d22e363
Opio Maxwell <opiomaxwell11122@gmail.com>
2023-04-18 11:01:22 +0300

    build the listing details screen

--- App.js
+++ App.js
@@ -4,13 +4,14 @@ import WelcomeScreen from "./app/screens/WelcomeScreen";
 import ViewImageScreen from "./app/screens/ViewImageScreen";
 import AppButton from "./app/components/AppButton";
 import colors from "./app/config/colors";
+import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
 
 export default function App() {
   const handlePress = () => {
     console.log("button Pressed");
   };
 
-  return <WelcomeScreen />;
+  return <ListingDetailsScreen />;
 }
 
 const styles = StyleSheet.create({});
---
+++ app/screens/ListingDetailsScreen.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "react-native";
+
+function ListingDetailsScreen(props) {
+  return <Text>ListingDetailsScreen</Text>;
+}
+
+export default ListingDetailsScreen;
