1ce4830526062b895e909e070a384dd2826ce7fb
Opio Maxwell <opiomaxwell11122@gmail.com>
2023-04-19 17:31:31 +0300

    add the modal to work with the picker component

--- app/components/AppPicker.js
+++ app/components/AppPicker.js
@@ -1,24 +1,37 @@
-import React from "react";
-import { TextInput, View, StyleSheet } from "react-native";
+import React, { useState } from "react";
+import { TextInput, View, StyleSheet, Modal, Button } from "react-native";
 import colors from "../config/colors";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import defaultStyles from "../config/styles";
 import AppText from "../components/AppText";
+import { TouchableWithoutFeedback } from "react-native";
+import Screen from "./Screen";
 
 function AppPicker({ icon, placeholder, ...otherProps }) {
-  return (
-    <View style={styles.container}>
-      {icon && (
-        <MaterialCommunityIcons style={styles.icon} size={20} name={icon} />
-      )}
-      <AppText style={styles.text}>{placeholder}</AppText>
+  const [modalVisible, setModalVisible] = useState(false);
 
-      <MaterialCommunityIcons
-        style={{ color: colors.medium }}
-        size={20}
-        name="chevron-down"
-      />
-    </View>
+  return (
+    <>
+      <TouchableWithoutFeedback onPress={() => setModalVisible(true)}>
+        <View style={styles.container}>
+          {icon && (
+            <MaterialCommunityIcons style={styles.icon} size={20} name={icon} />
+          )}
+          <AppText style={styles.text}>{placeholder}</AppText>
+
+          <MaterialCommunityIcons
+            style={{ color: colors.medium }}
+            size={20}
+            name="chevron-down"
+          />
+        </View>
+      </TouchableWithoutFeedback>
+      <Modal visible={modalVisible} animationType="slide">
+        <Screen>
+          <Button title="close" onPress={() => setModalVisible(false)} />
+        </Screen>
+      </Modal>
+    </>
   );
 }
 
