77329b133a87c50f3858e3d75dfc2eddb841000d
Opio Maxwell <opiomaxwell11122@gmail.com>
2023-04-19 10:20:25 +0300

    add app picker component

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StyleSheet } from "react-native";
 import Screen from "./app/components/Screen";
 import AppTextInput from "./app/components/AppTextInput";
+import AppPicker from "./app/components/AppPicker";
 
 export default function App() {
   return (
     <Screen>
-      <AppTextInput icon="email" placeholder="Username" />
+      <AppPicker />
     </Screen>
   );
 }
---
+++ app/components/AppPicker.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+function AppPicker(props) {
+  return (
+    <View>
+      <Text>App Picker</Text>
+    </View>
+  );
+}
+
+export default AppPicker;
