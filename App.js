583ace5fc479ba127de8abd23e0f0e2093581c2a
Opio Maxwell <opiomaxwell11122@gmail.com>
2023-04-19 10:17:39 +0300

    optimize imports in app.js

--- App.js
+++ App.js
@@ -1,13 +1,4 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, TextInput, View } from "react-native";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import ViewImageScreen from "./app/screens/ViewImageScreen";
-import AppButton from "./app/components/AppButton";
-import colors from "./app/config/colors";
-import ListingDetailsScreen from "./app/screens/ListingDetailsScreen";
-import MessagesScreen from "./app/screens/MessagesScreen";
-import MyAccountScreen from "./app/screens/MyAccountScreen";
-import ListingsScreen from "./app/screens/ListingsScreen";
+import { StyleSheet } from "react-native";
 import Screen from "./app/components/Screen";
 import AppTextInput from "./app/components/AppTextInput";
 
