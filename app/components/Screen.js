3a5011431d3fb7d86c3658e953a76c760fbd67e3
Opio Maxwell <opiomaxwell11122@gmail.com>
2023-04-20 22:26:12 +0300

    add the jsconfig.json to the root of the project

--- app/components/Screen.js
+++ app/components/Screen.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Constants from "expo-constants";
-import { SafeAreaView, StyleSheet, View } from "react-native";
+import { StyleSheet, View } from "react-native";
 
 export default function Screen({ children, style }) {
-  return <SafeAreaView style={[styles.screen, style]}>{children}</SafeAreaView>;
+  return <View style={[styles.screen, style]}>{children}</View>;
 }
 
 const styles = StyleSheet.create({
